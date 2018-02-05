0f5c1e3e9ed05e5595e5e98245f47f1353c58595
Carl Saunders <carl.saunders@deadlybyte.co.uk>
2018-02-05 20:38:12 +0000

    Fixed bug - temperature was not rendering correctly when value was 0 - update logic from falsy

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -9,7 +9,7 @@ const Weather = (props) => {
     <span className="CM-weather-forecast">
       Weather - {forecast ? forecast : 'Unknown'}
       {
-        temperature &&
+        (temperature !== null || temperature !== undefined) &&
         <span>, {temperature}&deg;C</span>
       }
     </span>
