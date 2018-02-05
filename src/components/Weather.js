7e856369b974eec1fc5054d2f205662e1cffa619
Carl Saunders <carl.saunders@deadlybyte.co.uk>
2018-02-05 20:47:33 +0000

    Fixed bug - incorrect logic for rendering temperature (|| instead of &&)

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -9,7 +9,7 @@ const Weather = (props) => {
     <span className="CM-weather-forecast">
       Weather - {forecast ? forecast : 'Unknown'}
       {
-        (temperature !== null || temperature !== undefined) &&
+        (temperature !== undefined && temperature !== null) &&
         <span>, {temperature}&deg;C</span>
       }
     </span>
